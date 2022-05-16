a16f073ff36e1723a8e93bb526390a791bb7c918
Kookiz6 <105597210+Kookiz6@users.noreply.github.com>
2022-05-16 21:16:24 +0200

    Add files via upload

---
+++ noseqhago.java
@@ -0,0 +1,13 @@
+
+import java.util.ArrayList;
+
+public class noseqhago {
+    public static void main(String [] args) {
+        int resultado = 0;
+        resultado = suma( 15, 50);
+        System.out.println(resultado);
+    }
+    public static int suma(int a, int b) {
+        return a + b;
+    }
+}
